cc8bff30f61e42090d7f06047466ccedde562cbf
Julian <julian.b.shah@gmail.com>
2019-12-09 14:02:22 -0600

    Basic abstraction stuff - less hardware maps in code, etc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class DriveBase {
+
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;;
+    public DcMotor backRight;
+
+    public DriveBase(HardwareMap hardwareMap, Telemetry telemetry) {
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
@@ -1,7 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class MainRobot {
 
+    public DriveBase driveBase;
 
+    public DcMotor lifter;
+    public CRServo extender;
+
+    private Servo rotator;
+    private CRServo grabber;
+
+    private Servo leftPlatform;
+    private Servo rightPlatform;
+
+    private Servo leftSlapper;
+    private Servo rightSlapper;
+
+    private DigitalChannel extenderSwitch;
+    private DigitalChannel lifterSwitch;
+
+    public MainRobot(HardwareMap hardwareMap, Telemetry telemetry) {
+
+        driveBase = new DriveBase(hardwareMap, telemetry);
+
+        lifter = hardwareMap.get(DcMotor.class, "lifter");
+        extender = hardwareMap.get(CRServo.class, "extender");
+
+        rotator = hardwareMap.get(Servo.class, "rotator");
+        grabber = hardwareMap.get(CRServo.class, "grabber");
+
+        leftPlatform = hardwareMap.get(Servo.class, "leftPlatform");
+        rightPlatform = hardwareMap.get(Servo.class, "rightPlatform");
+
+        leftSlapper = hardwareMap.get(Servo.class, "leftSlapper");
+        rightSlapper = hardwareMap.get(Servo.class, "rightSlapper");
+
+        extenderSwitch = hardwareMap.get(DigitalChannel.class, "extenderLimitSwitch");
+        lifterSwitch = hardwareMap.get(DigitalChannel.class, "lifterLimitSwitch");
+
+        leftPlatform.setDirection(Servo.Direction.REVERSE);
+        rightPlatform.setDirection(Servo.Direction.FORWARD);
+
+        lifter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rotator.setPosition(0.5);
+
+    }
 
 }
